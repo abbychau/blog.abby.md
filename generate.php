be3fce4fc3075442c2cff6769f57021d438a3574
Abby <i@abby.md>
2021-07-13 23:10:08 +0900

    renew blog to old-day frameset

--- generate.php
+++ generate.php
@@ -2,12 +2,12 @@
 
 <?php
 include("./vendor/autoload.php");
-// $Parsedown = new Parsedown();
+$Parsedown = new Parsedown();
 use Symfony\Component\Yaml\Yaml;
 use WyriHaximus\HtmlCompress\Factory;
 
-
 function process($file){
+    global $Parsedown;
     $txt=file_get_contents($file);
     $ytxt=[];$mc=0;$store=[];
     foreach(explode("\n", $txt) as $line){
@@ -24,15 +24,27 @@ function process($file){
                 if(is_numeric($data['date'])){
                     $data['date'] = date("Y-m-d",$data['date']);
                 }
-                return $data;
-                break;
+                $ytxt=[];
+                // return $data;
+                // break;
             }
             continue;
         }
         $ytxt[]=$line;
-        
     }
-    return null;
+    $content=implode("\n",$ytxt);
+    $markdownContent = $Parsedown->text($content);
+    $cleanPath = "_generated/".trim(str_replace(['/','.md'],['_','.htm'],$data['path']),"._");
+    $template = file_get_contents('templates/template_article.htm');
+    $template = str_replace(
+        ["{{subject}}","{{date}}","{{tags}}","{{markdown}}","{{paramlink}}"],
+        [$data['title'],$data['date'],
+        (isset($data['tags'])?json_encode($data['tags'],JSON_UNESCAPED_UNICODE):"").json_encode($data['categories'],JSON_UNESCAPED_UNICODE),$markdownContent,"https://blog.abby.md/{$cleanPath}"],
+        $template
+    );
+    file_put_contents($cleanPath,$template);
+    $out=['meta'=>$data,'content'=>$content,'markdown'=>$markdownContent,'generated_path'=>$cleanPath];
+    return $out;
 }
 echo "Loading Files...\n";
 foreach(glob("./_posts/*") as $file){
@@ -47,28 +59,28 @@ foreach(glob("./realblog/*/*") as $v){
     $store[]=process($file);
 }
 usort($store,function($a,$b){
-    return $b['date'] <=> $a['date'];
+    return $b['meta']['date'] <=> $a['meta']['date'];
 });
 $list='';
 $prev07='';
 echo "Generating List...\n";
 foreach($store as $v){
-    $sub07=substr($v['date'],0,7);
+    $sub07=substr($v['meta']['date'],0,7);
     $head = '';
     if($sub07 != $prev07){
         $prev07 = $sub07;
         $head = "$sub07<br />";
     }
-    $title=$v['title']?$v['title']:'untitled';
-    $list.="$head &gt; <a onclick=\"go('{$v['path']}')\">{$title}</a><br />";
+    $title=$v['meta']['title']?$v['meta']['title']:'untitled';
+    $list.="$head &gt; <a href='{$v['generated_path']}' target='main_frame'>{$title}</a><br />";
 }
-$template = file_get_contents('template.htm');
+$template = file_get_contents('templates/template_list.htm');
 $template = str_replace("{{list}}",$list,$template);
-echo "Compressing...\n";
-$parser = Factory::constructSmallest();
-$compressedHtml = $parser->compress($template);
+// echo "Compressing...\n";
+// $parser = Factory::constructSmallest();
+// $compressedHtml = $parser->compress($template);
 
-file_put_contents("index.htm",$compressedHtml);
+file_put_contents("list.htm",$template);
 
 //jf1.1
 // {
@@ -95,7 +107,7 @@ $jsonfeed['title']="Abby's Archive";
 $jsonfeed['home_page_url']="https://blog.abby.md";
 $jsonfeed['feed_url']="https://blog.abby.md/feed.json";
 foreach($store as $v){
-    $jsonfeed['items'][]=['id'=>md5($v['date']), 'url'=>"https://blog.abby.md/#{$v['path']}", 'title'=>$v['title'] ];
+    $jsonfeed['items'][]=['id'=>md5($v['meta']['date']), 'url'=>"https://blog.abby.md/{$v['generated_path']}", 'title'=>$v['meta']['title'] ];
 }
 file_put_contents("feed.json",json_encode($jsonfeed));
 echo "Done.\n";
